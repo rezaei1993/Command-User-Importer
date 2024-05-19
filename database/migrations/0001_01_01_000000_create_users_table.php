34dd1c027b6e8772d112806818a311ead751117e
rezaei1993 <d.rezaei1993@gmail.com>
2024-05-19 12:17:23 +0330

    feat(user-importer): Initialize laravel project

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('national_code', 10)->unique();
+            $table->string('phone_number', 15)->unique();
+            $table->timestamps();
+
+            $table->unique('national_code');
+            $table->unique('phone_number');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
